5d55653f5956757e185c41c55eef231eefaf96f1
SaInekK <mober228lol@gmail.com>
2022-01-11 18:07:18 +0300

    Gif added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # life_game
 # Game of life Flask project
+Zero-player game implementation of cellular automaton devised by the British mathematician John Horton Conway.
+![](gof.gif)
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from project.forms import MessageForm
 from game_of_life import GameOfLife
+import os
 
 app = Flask(__name__)
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/index')
-@app.route('/')
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET KEY") or "any_key"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/index', methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def index():
+    height = 25
+    width = 25
+    if request.method == 'POST':
+        height = request.form.get('height')
+        width = request.form.get('width')
+        if height.isdigit() and width.isdigit():
+            GameOfLife(int(height), int(width)).generate_universe()
+            return redirect(url_for('life'))
+        else:
+            flash('Enter 2 numbers')
     GameOfLife(25, 25).generate_universe()
     return render_template('index.html')
 
@@ -29,6 +49,21 @@ def stat():
         game.form_new_generation()
     game.generation += 1
     return render_template('life-static.html.j2', world=game.world, old_world=game.old_world, counter=game.generation)
+
+
+@app.route('/message', methods=["POST", "GET"])
+@app.route('/message/', methods=["POST", "GET"])
+def message():
+    form = MessageForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        print(name, email, message, sep='/n')
+        print("\nData received. Now redirecting...")
+        return redirect(url_for('message'))
+
+    return render_template('message.html.j2', form=form)
 
 
 if __name__ == "__main__":  # Запуск вебсервера
